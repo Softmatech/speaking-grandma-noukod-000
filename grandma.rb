20e0cf735b634705497e255c4a0e8488da1d56f6
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-16 18:12:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 
 def speak_to_grandma(phrase)
     if phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
+      return "I LOVE YOU TOO PUMPKIN!"         #{}
     elsif phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
+      return "NO, NOT SINCE 1938!"
     else
       return "HUH?! SPEAK UP, SONNY!"
     end
