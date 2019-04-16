fbbdd30c046655177812433ba103661d2796ee25
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-16 18:22:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(phrase)
     else if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == "WHAT?"
-        return == "NO, NOT SINCE 1938!"
+        return "NO, NOT SINCE 1938!"
       else
       return "HUH?! SPEAK UP, SONNY!"
     end
