6d9f3b13475e298de71d4d2f5d3bdb364cea8748
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-16 15:34:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 
 
 def speak_to_grandma(phrase)
-    if !phrase.uppercase
+    if !phrase.upcase
       puts "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == "I LOVE YOU GRANDMA!"
+    elsif phrase.upcase == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == "NO, NOT SINCE 1938!"
+    elsif phrase.upcase == "NO, NOT SINCE 1938!"
       puts "WHAT?"
     end
 end
