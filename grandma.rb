0b8f1e3218d71312ba5ba362f6c5ad31f2cc41e2
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-16 16:27:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU GRANDMA!
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else 
+  else
     return "NO, NOT SINCE 1938!"
   end
 end
